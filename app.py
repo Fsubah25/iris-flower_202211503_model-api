147afe1112298dce45973f9b05b46dd21f922107
Fsubah25 <michaelsubah23@gmail.comgit config --global user.name Fsubah25git config  user.email michaelsubah23@gmail.com>
2025-06-24 09:13:33 -0700

    Updated files

--- app.py
+++ app.py
@@ -69,5 +69,8 @@ def predict():
                                   feature_names=feature_names,
                                   target_names=target_names)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
